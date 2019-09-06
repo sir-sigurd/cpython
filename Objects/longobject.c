1864b2063b0c501dabc00d5321ea9977da1c3fec
hongweipeng <961365124@qq.com>
2019-09-06 17:20:00 +0800

    small_ints[x] could be returned in long_add and long_sub

--- Objects/longobject.c
+++ Objects/longobject.c
@@ -3277,7 +3277,7 @@ long_add(PyLongObject *a, PyLongObject *b)
         else
             z = x_add(a, b);
     }
-    return (PyObject *)z;
+    return (PyObject *)maybe_small_long(z);
 }
 
 static PyObject *
@@ -3306,7 +3306,7 @@ long_sub(PyLongObject *a, PyLongObject *b)
         else
             z = x_sub(a, b);
     }
-    return (PyObject *)z;
+    return (PyObject *)maybe_small_long(z);
 }
 
 /* Grade school multiplication, ignoring the signs.
